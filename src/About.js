22c671aedb1ec02e1dd7c027d57509bbff0153f8
vinaykumar4312 <vinaykumarkomakula@gmail.com>
2022-01-22 23:58:49 +0530

    Changing message

---
+++ src/About.js
@@ -0,0 +1,45 @@
+import { Col, Container, Row, Image, Button } from "react-bootstrap";
+const About = () => {
+  return (
+    <div className="about">
+      <Container>
+        <Row>
+          <Col>
+            <div className="abt-img">
+              <Image
+                src={process.env.PUBLIC_URL + "./images/vinaykumar.jpg"}
+                roundedCircle
+                style={{
+                  border: "10px solid #444",
+                  boxShadow: "0 0 25px 0 rgba(0, 0, 0, .8)",
+                }}
+              />
+            </div>
+          </Col>
+          <Col>
+            <div className="abt-content">
+              <span>Frontend developer</span>
+              <h1>Vinaykumar Komakula</h1>
+              <p>
+                I think you’ve got a good idea about Complete Web Solution. I‘m
+                an expert freelance Web Designer and Developer and Digital
+                Marketing in Hyderabad.
+              </p>
+              <Button
+                variant="outline-primary"
+                style={{ borderRadius: "20px", marginRight: "10px" }}
+              >
+                Download CV
+              </Button>{" "}
+              <Button variant="outline-dark" style={{ borderRadius: "20px" }}>
+                Hire Me
+              </Button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import About from "./About";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Github Added React Project</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <About />
     </div>
   );
 }
