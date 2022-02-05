e167bb4045f184cd1ab4ba42290aef42c53e21c5
vinaykumar4312 <vinaykumarkomakula@gmail.com>
2022-02-05 22:41:55 +0530

    Changing message

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,30 +1,7 @@
-import { useState } from "react";
 import { Container } from "react-bootstrap";
 import Slider from "react-slick";
 
 const Testimonial = () => {
-  const [testimonials, setTestimonials] = useState([
-    {
-      name: "manoj",
-      body: "Mauris neque libero, aliquet vel mollis nec, euismod sed tellus. Mauris convallis dictum elit id volutpat. Vivamus blandit, dolor vitae lacinia maximus, risus velit vehicula odio, a tincidunt turpis turpis tempus ex.",
-      id: 1,
-    },
-    {
-      name: "ravi",
-      body: "Mauris neque libero, aliquet vel mollis nec, euismod sed tellus. Mauris convallis dictum elit id volutpat. Vivamus blandit, dolor vitae lacinia maximus, risus velit vehicula odio, a tincidunt turpis turpis tempus ex.",
-      id: 2,
-    },
-    {
-      name: "ramesh",
-      body: "Mauris neque libero, aliquet vel mollis nec, euismod sed tellus. Mauris convallis dictum elit id volutpat. Vivamus blandit, dolor vitae lacinia maximus, risus velit vehicula odio, a tincidunt turpis turpis tempus ex.",
-      id: 3,
-    },
-    {
-      name: "gopi",
-      body: "Mauris neque libero, aliquet vel mollis nec, euismod sed tellus. Mauris convallis dictum elit id volutpat. Vivamus blandit, dolor vitae lacinia maximus, risus velit vehicula odio, a tincidunt turpis turpis tempus ex.",
-      id: 4,
-    },
-  ]);
   const settings = {
     dots: true,
     infinite: true,
@@ -39,14 +16,50 @@ const Testimonial = () => {
       <Container>
         <h2 className="testi">Testimonials</h2>
         <Slider {...settings}>
-          {testimonials.map((testimonial) => (
-            <div className="card-box" key={testimonial.id}>
-              <div className="card-body">
-                <p>{testimonial.body}</p>
-                <h5>{testimonial.name}</h5>
-              </div>
+          <div className="card-box">
+            <div className="card-body">
+              <p>
+                Mauris neque libero, aliquet vel mollis nec, euismod sed tellus.
+                Mauris convallis dictum elit id volutpat. Vivamus blandit, dolor
+                vitae lacinia maximus, risus velit vehicula odio, a tincidunt
+                turpis turpis tempus ex.
+              </p>
+              <h5>manoj</h5>
             </div>
-          ))}
+          </div>
+          <div className="card-box">
+            <div className="card-body">
+              <p>
+                Mauris neque libero, aliquet vel mollis nec, euismod sed tellus.
+                Mauris convallis dictum elit id volutpat. Vivamus blandit, dolor
+                vitae lacinia maximus, risus velit vehicula odio, a tincidunt
+                turpis turpis tempus ex.
+              </p>
+              <h5>ravi</h5>
+            </div>
+          </div>
+          <div className="card-box">
+            <div className="card-body">
+              <p>
+                Mauris neque libero, aliquet vel mollis nec, euismod sed tellus.
+                Mauris convallis dictum elit id volutpat. Vivamus blandit, dolor
+                vitae lacinia maximus, risus velit vehicula odio, a tincidunt
+                turpis turpis tempus ex.
+              </p>
+              <h5>ramesh</h5>
+            </div>
+          </div>
+          <div className="card-box">
+            <div className="card-body">
+              <p>
+                Mauris neque libero, aliquet vel mollis nec, euismod sed tellus.
+                Mauris convallis dictum elit id volutpat. Vivamus blandit, dolor
+                vitae lacinia maximus, risus velit vehicula odio, a tincidunt
+                turpis turpis tempus ex.
+              </p>
+              <h5>gopi</h5>
+            </div>
+          </div>
         </Slider>
       </Container>
     </div>
