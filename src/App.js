b669112ebb0946f2171ce6e3bfb69a0bd25c257b
vinaykumar4312 <vinaykumarkomakula@gmail.com>
2022-02-05 21:53:13 +0530

    Changing message

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import About from "./About";
 import NavBar from "./components/NavBar";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <About />
+      <Footer />
     </div>
   );
 }
